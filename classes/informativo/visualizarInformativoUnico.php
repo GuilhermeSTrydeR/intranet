7ef4e3f802a4e875594bdd3d3a6cc8be39b98b7c
GuilhermeSTrydeR <guilhermepereiramello@hotmail.com>
2021-05-13 12:49:29 -0300

    ajustado lead-out de impressao dos informativos unicos

--- classes/informativo/visualizarInformativoUnico.php
+++ classes/informativo/visualizarInformativoUnico.php
@@ -38,12 +38,11 @@
         while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
             if($linha['id'] == $id){
 
-            echo"<br>";
 
             echo "<div class='mural'>";
             
             echo "<center>";  
-            echo "<table class='table' style='background-color: #ffffff;  border-top-left-radius: 40px; border-top-right-radius: 40px; border-bottom-left-radius: 40px; ' table-layout:fixed;  max-width: 1200px; word-wrap: break-word; !important; margin-left: 200px;'>";
+            echo "<table id='tblInfUnico'>";
         
           
             $linha['dataCadastro'] = gmdate("d/m/y á\s\ H:i", $linha['dataCadastro']);
@@ -52,19 +51,20 @@
                 
             
             echo "<tr>";
-            echo "<td style='border: none; max-width: 500px;'><br><center><b><p style='float: left; margin-left: 250px; color: #F47920;'>Publicado em: {$linha['dataCadastro']}</p></b></center><br><br><center><h3 style='color: #009b63;  word-wrap: break-word; max-width: 1050px; margin-left: 200px;'> {$linha['titulo']}</h3></center> </td>";
+            echo "<td ><br><center><b><p >Publicado em: {$linha['dataCadastro']}</p></b></center><br><br><center><h3> {$linha['titulo']}</h3><br></center> </td>";
             echo "</tr>";
-
-            echo "<tr>";
             
-            echo nl2br("<td style='border: none;'><p style='word-wrap: break-word; max-width: 1000px; padding: 15px; margin-left: 270px; color: black !important;'>{$linha['texto']}</p></td>");
+            echo "<tr>";
+       
+            
+            echo nl2br("<td><p id='texto'>{$linha['texto']}</p></td>");
 
             echo "</tr>";
 
                 echo "<tr>";
                 echo "<td>";
                 echo "<center>";
-                echo "<img onMouseOver='aumenta(this)' onMouseOut='diminui(this)' class='imagem' style='max-width: 800px; margin-left: 200px;' src='" . $linha['imagem'] ."'></img>";
+                echo "<img id='img' onMouseOver='aumenta(this)' onMouseOut='diminui(this)' src='" . $linha['imagem'] ."'></img>";
                 echo"<div class='row' style='height: 100px;'></div>";
                 echo "</center>";
                 echo "</td>";
