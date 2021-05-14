9ebf220a76cfebebea43f77902bcf0fd180ef0c1
GuilhermeSTrydeR <guilhermepereiramello@hotmail.com>
2021-05-14 13:08:19 -0300

    alterado regra de login para naop aceitar usuarios desativados

--- classes/usuario/logar.php
+++ classes/usuario/logar.php
@@ -31,26 +31,61 @@
         $_SESSION['id'] = $u->id($user);
         
         // BLOCO DE LOGIN USUARIO COMUM
-        if($u->login($user, $pass) == true && $u->permissao($user) == 1 && $u->verificaAtivo($id) == 1){
+        if($u->login($user, $pass) == true && $u->permissao($user) == 1 && $u->verificaExclusao($user) == 0){
 
-            $_SESSION['logado'] = 1;
-            header("location: ../../paginas/comum/main.php");
+            if($u->verificaAtivo($id) == 1){
+
+                $_SESSION['logado'] = 1;
+                header("location: ../../paginas/comum/main.php");
+
+
+            }
+            else{
+                echo "<script>alert('Usuário desativado!, por favor entre em contato com o Administrador do sistema!');</script>";
+                $url = '../../index.php';
+                echo'<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
+
+            }
         
         }
 
         // BLOCO DE LOGIN USUARIO SUPERVISOR
-        if($u->login($user, $pass) == true && $u->permissao($user) == 2 && $u->verificaExclusao($user) == 0 && $u->verificaAtivo($id) == 1){
+        elseif($u->login($user, $pass) == true && $u->permissao($user) == 2 && $u->verificaExclusao($user) == 0 ){
 
-            $_SESSION['logado'] = 1;
-            header("location: ../../paginas/supervisor/main.php");
+            if($u->verificaAtivo($id) == 1){
+
+                $_SESSION['logado'] = 1;
+                header("location: ../../paginas/supervisor/main.php");
+
+
+            }
+            else{
+                echo "<script>alert('Usuário desativado!, por favor entre em contato com o Administrador do sistema!');</script>";
+                $url = '../../index.php';
+                echo'<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
+
+            }
             
         }
 
         //BLOCO DE LOGIN USUARIO ADMINISTRADOR
-        if($u->login($user, $pass) == true && $u->permissao($user) == 3 && $u->verificaAtivo($id) == 1 && $u->verificaExclusao($user) == 0 ){
+        elseif($u->login($user, $pass) == true && $u->permissao($user) == 3  && $u->verificaExclusao($user) == 0 ){
             
-            $_SESSION['logado'] = 1;
-            header("location: ../../paginas/admin/main.php");
+            if($u->verificaAtivo($id) == 1){
+
+                $_SESSION['logado'] = 1;
+                header("location: ../../paginas/admin/main.php");
+
+
+            }
+            else{
+                echo "<script>alert('Usuário desativado!, por favor entre em contato com o Administrador do sistema!');</script>";
+                $url = '../../index.php';
+                echo'<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
+
+            }
+
+            
             
         }
    
